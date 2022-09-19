560fe726b45f8890c0476e388c9b700e1d586457
T-naul <pvluan225@gmail.com>
2022-09-19 16:02:01 +0700

    change

--- buoi2/sql_query_SUBQUERIES.sql
+++ buoi2/sql_query_SUBQUERIES.sql
@@ -10,12 +10,10 @@
 -- FROM employees
 -- WHERE department_id = 60;
 -- 3. Write a query to find the names (first_name, last_name) of the employees who have a manager who works for a department based in the United States.
+-- WITH depart AS (SELECT department_id FROM department WHERE location_id = 1600 OR location_id = 1700)
 -- SELECT first_name || ' ' || last_name as name
 -- FROM employees
--- WHERE manager_id in (
--- SELECT employee_id
--- FROM employees
--- WHERE department_id in (SELECT department_id FROM department WHERE location_id = 1600 OR location_id = 1700));
+-- WHERE manager_id in (SELECT employee_id FROM employees WHERE department_id in (SELECT * FROM depart));
 
 -- 4. Write a query to find the names (first_name, last_name) of the employees who are managers.
 -- SELECT first_name || ' ' || last_name as name
@@ -25,10 +23,11 @@
 -- SELECT first_name || ' ' || last_name as name
 -- FROM employees
 -- WHERE salary > (SELECT SUM(salary)/COUNT(salary) as salary FROM employees);
--- 6. Write a query to find the names (first_name, last_name), the salary of the employees whose salary is equal to the minimum salary for their job grade. 
+-- 6. Write a query to find the names (first_name, last_name), the salary of the employees whose salary is equal to the minimum salary for their job grade.
+-- WITH min_sala as (SELECT MIN(salary) as min_salary, job_id FROM employees GROUP BY job_id)
 -- SELECT first_name || ' ' || last_name as name, salary, employees.job_id
 -- FROM employees
--- JOIN (SELECT MIN(salary) as min_salary, job_id FROM employees GROUP BY job_id) as b on employees.salary = b.min_salary AND employees.job_id = b.job_id;
+-- JOIN min_sala  on employees.salary = min_sala.min_salary AND employees.job_id = min_sala.job_id;
 
 -- 7. Write a query to find the names (first_name, last_name), the salary of the employees who earn more than the average salary and who works in any of the IT departments. 
 -- SELECT first_name || ' ' || last_name as name, salary
@@ -39,11 +38,14 @@
 -- FROM employees
 -- WHERE salary > (SELECT salary FROM employees WHERE last_name = 'Bell') ;
 -- 9. Write a query to find the names (first_name, last_name), the salary of the employees who earn the same salary as the minimum salary for all departments.
--- SELECT first_name || ' ' || last_name as name, salary
--- FROM employees
--- WHERE salary > (SELECT salary FROM employees WHERE last_name = 'Bell') ;
+WITH min_sala as (SELECT MIN(salary) as min_salary, job_id FROM employees GROUP BY job_id)
+SELECT first_name || ' ' || last_name as name, salary, employees.job_id
+FROM employees
+JOIN min_sala  on employees.salary = min_sala.min_salary AND employees.job_id = min_sala.job_id;
 -- 10. Write a query to find the names (first_name, last_name) of the employees who are not supervisors.
-
+-- SELECT first_name || ' ' || last_name as name
+-- FROM employees
+-- WHERE employee_id NOT IN (SELECT DISTINCT manager_id FROM employees);
 -- 11. Write a query to display the employee ID, first name, last names, salary of all employees whose salary is above average for their departments.
 -- SELECT first_name || ' ' || last_name as name, salary, employees.job_id
 -- FROM employees
@@ -67,7 +69,7 @@
 -- 17. Write a query to get 3 minimum salaries.
 --SELECT * FROM employees ORDER BY salary ASC LIMIT 3;
 -- 18. Write a query to get nth max salaries of employees.
-SELECT *
-FROM employees emp1
-WHERE (0) = (SELECT COUNT(DISTINCT(emp2.salary)) FROM employees emp2 WHERE emp2.salary > emp1.salary);
+-- SELECT *
+-- FROM employees emp1
+-- WHERE (0) = (SELECT COUNT(DISTINCT(emp2.salary)) FROM employees emp2 WHERE emp2.salary > emp1.salary);
 
