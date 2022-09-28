50a3b03105d877453efd576cce0fc4200407ee48
T-naul <pvluan225@gmail.com>
2022-09-28 19:13:45 +0700

    Commit

--- buoi2/Assignment2/asigm_1_proj/forms.py
+++ buoi2/Assignment2/asigm_1_proj/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 class AddUser(forms.Form):
     name = forms.CharField(label='Name',max_length=50)
-    age = forms.CharField(label='Age')
+    age = forms.IntegerField(label='Age')
     address = forms.CharField(label='Address')
     mobile_number = forms.CharField(label='Mobile number',max_length=10)
